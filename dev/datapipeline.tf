355069de2ed20a59af044acad9024dd6a42bb02c
seogaag <oisge@naver.com>
2024-08-23 00:04:25 +0900

    update dp:

--- dev/datapipeline.tf
+++ dev/datapipeline.tf
@@ -5,7 +5,7 @@ provider "aws" {
 ## 데이터 업로드
 
 resource "aws_s3_bucket" "s3_stock" {
-  bucket = "esia-stock-test"
+  bucket = "esia-stock-test-j"
 }
 
 resource "aws_s3_object" "s3_stock_raw_data" {
@@ -68,6 +68,7 @@ module "lambda_collector" {
   lambda_env = {
     API_KEY="QE0t8vHW3Ndx82q30U_qiZMOQc0kRrl6"
   }
+  sagemaker_role_arn = module.sagemaker.sagemaker_role_arn
 }
 
 module "lambda_preprocess" {
@@ -84,6 +85,7 @@ module "lambda_preprocess" {
     PREFIXES_LIST ="AAPL/, NVDA/"
   }
 
+  sagemaker_role_arn = module.sagemaker.sagemaker_role_arn
 }
 
 module "lambda_training" {
@@ -98,16 +100,28 @@ module "lambda_training" {
     BUCKET_NAME = aws_s3_bucket.s3_stock.bucket
     SAGEMAKER_ROLE = module.sagemaker.sagemaker_role_arn
   }
-  
+  sagemaker_role_arn = module.sagemaker.sagemaker_role_arn
 }
 
-resource "aws_lambda_layer_version" "lambda_layer_numpy" {
-  description         = "Example lambda layer"
-  filename            = "../source/lambda_layer_numpy.zip"
-  layer_name          = "lambda_layer_numpy"
-  compatible_runtimes = ["python3.8"]
-  source_code_hash    = filebase64sha256("../source/lambda_layer_numpy.zip")
-}
+# module "lambda_waittraining" {
+#   source = "../modules/lambda"
+
+#   service = "esia-wait"
+#   bucket_arn = aws_s3_bucket.s3_stock.arn
+#   source_path = "../source"
+#   lambda_function_name = "ESIA_waiting"
+#   lambda_file_name = "lambda_waittraining"
+#   lambda_env = {}
+#   sagemaker_role_arn = module.sagemaker.sagemaker_role_arn
+# }
+
+# resource "aws_lambda_layer_version" "lambda_layer_numpy" {
+#   description         = "Example lambda layer"
+#   filename            = "../source/lambda_layer_numpy.zip"
+#   layer_name          = "lambda_layer_numpy"
+#   compatible_runtimes = ["python3.8"]
+#   source_code_hash    = filebase64sha256("../source/lambda_layer_numpy.zip")
+# }
 
 
 module "lambda_evaluation" {
@@ -118,19 +132,14 @@ module "lambda_evaluation" {
   source_path = "../source"
   lambda_function_name = "ESIA_evaluation"
   lambda_file_name = "lambda_evaluation"
-  lamda_layer_arns = [aws_lambda_layer_version.lambda_layer_numpy.arn]
+  # lamda_layer_arns = [aws_lambda_layer_version.lambda_layer_numpy.arn]
   lambda_env = {
     BUCKET_NAME = aws_s3_bucket.s3_stock.bucket
+    SAGEMAKER_ROLE = module.sagemaker.sagemaker_role_arn
   }
   timeout = 900
+  sagemaker_role_arn = module.sagemaker.sagemaker_role_arn
 }
-# resource "aws_lambda_function" "model_evaluation" {
-#   filename         = "../source/lambda_model_evaluation_payload.zip"
-#   function_name    = "ModelEvaluationFunction"
-#   role             = aws_iam_role.lambda_execution_role.arn
-#   handler          = "index.handler"
-#   runtime          = "python3.8"
-# }
 
 # resource "aws_lambda_function" "model_inference" {
 #   filename         = "lambda_model_inference_payload.zip"
@@ -167,14 +176,22 @@ resource "aws_sfn_state_machine" "workflow" {
       TrainModel = {
         Type = "Task",
         Resource = module.lambda_training.lambda_function_arn,
-        Next = "EvaluateModel",
+        Next = "Wait",
         Parameters: {
           "companies": ["AAPL", "NVDA"]
         }
       },
+      Wait = {
+        Type = "Wait",
+        Seconds = 480,  # 8분 동안 대기
+        Next = "EvaluateModel"
+      },
       EvaluateModel = {
         Type = "Task",
         Resource = module.lambda_evaluation.lambda_function_arn,
+        Parameters: {
+          "companies": ["AAPL", "NVDA"]
+        },
         End = true
       }
 #       GeneratePrediction = {
@@ -190,3 +207,98 @@ resource "aws_sfn_state_machine" "workflow" {
     module.lambda_training,
     module.lambda_evaluation ]
 }
+
+# resource "aws_sfn_state_machine" "workflow" {
+#   name     = "ModelTrainingAndEvaluation"
+#   role_arn = module.lambda_evaluation.lambda_role_arn
+
+#   definition = jsonencode({
+#     Comment = "State machine to preprocess data, train and evaluate model for multiple companies",
+#     StartAt = "CollectData",
+#     States = {
+#       CollectData = {
+#         Type = "Task",
+#         Resource = module.lambda_collector.lambda_function_arn,
+#         Next = "PreprocessData",
+#         Parameters = {
+#           "companies": ["AAPL", "NVDA"]
+#         }
+#       },
+#       PreprocessData = {
+#         Type = "Task",
+#         Resource = module.lambda_preprocess.lambda_function_arn,
+#         ResultPath = "$.PreprocessResult",  # Store the output of this task in $.PreprocessResult
+#         Next = "TrainModelForCompany",
+#         Parameters = {
+#           "companies": ["AAPL", "NVDA"]  # Pass the companies array to the lambda function
+#         }
+#       },
+#       TrainModelForCompany = {
+#         Type = "Map",
+#         Iterator = {
+#           StartAt = "TrainModel",
+#           States = {
+#             TrainModel = {
+#               Type = "Task",
+#               Resource = "arn:aws:states:::sagemaker:createTrainingJob.sync",
+#               Parameters = {
+#                 "TrainingJobName.$": "States.Format('ESIATrainingJob-{}-{}', $.company, $.date)",
+#                 "RoleArn": module.sagemaker.sagemaker_role_arn,
+#                 "AlgorithmSpecification": {
+#                   "TrainingImage": "991648021394.dkr.ecr.ap-south-1.amazonaws.com/forecasting-deepar:latest",
+#                   "TrainingInputMode": "File"
+#                 },
+#                 "InputDataConfig": [
+#                   {
+#                     "ChannelName": "train",
+#                     "DataSource": {
+#                       "S3DataSource": {
+#                         "S3DataType": "S3Prefix",
+#                         "S3Uri.$": "States.Format('s3://${aws_s3_bucket.s3_stock.bucket}/processed/{}/train.json', $.company)"
+#                       }
+#                     }
+#                   }
+#                 ],
+#                 "OutputDataConfig": {
+#                   "S3OutputPath.$": "States.Format('s3://${aws_s3_bucket.s3_stock.bucket}/model-output/{}', $.company)"
+#                 },
+#                 "ResourceConfig": {
+#                   "InstanceType": "ml.m4.xlarge",
+#                   "InstanceCount": 1,
+#                   "VolumeSizeInGB": 30
+#                 },
+#                 "StoppingCondition": {
+#                   "MaxRuntimeInSeconds": 3600
+#                 }
+#               },
+#               ResultPath = "$.TrainingResult",
+#               Next = "EvaluateModel"
+#             },
+#             EvaluateModel = {
+#               Type = "Task",
+#               Resource = module.lambda_evaluation.lambda_function_arn,
+#               Parameters = {
+#                 "TrainingResult.$": "$.TrainingResult",
+#                 "company.$": "$.company"
+#               },
+#               End = true
+#             }
+#           }
+#         },
+#         ItemsPath = "$.PreprocessResult.companies",  # Correctly reference the array of companies
+#         Parameters = {
+#           "company.$": "$$.Map.Item.Value",
+#           "date.$": "States.Format('{}', $$.Execution.Input.date)"
+#         },
+#         ResultPath = "$.TrainingResults",
+#         End = true
+#       }
+#     }
+#   })
+
+#   depends_on = [ 
+#     module.lambda_collector, 
+#     module.lambda_preprocess, 
+#     module.lambda_evaluation 
+#   ]
+# }
